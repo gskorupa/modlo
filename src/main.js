6f58b623f8eb121030492aacf9bfefce228d6f59
Grzegorz Skorupa <GSkorupa@nmg.pl>
2020-08-22 23:30:36 +0200

    wip

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ const app = new App({
 });
 
 if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/modlo/sw.js');
+    navigator.serviceWorker.register('./sw.js');
 };
 
 export default app;
