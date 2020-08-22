7e6632127f3cb880a79b8fd6859cc99b01993191
Grzegorz Skorupa <GSkorupa@nmg.pl>
2020-08-23 01:35:57 +0200

    wip

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ async function registerPeriodicNewsCheck() {
   const registration = await navigator.serviceWorker.ready;
   try {
     await registration.periodicSync.register('fetch-news', {
-      minInterval: 24 * 60 * 60 * 1000,
+      minInterval: 10 * 1000,
     });
   } catch {
     console.log('Periodic Sync could not be registered!');
